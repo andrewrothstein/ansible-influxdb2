6c45883f41c8e98cd5e871cc206b34a92769731e
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-12-27 11:45:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.influxdb2
+=========
+[![Build Status](https://travis-ci.org/andrewrothstein/ansible-influxdb2.svg?branch=master)](https://travis-ci.org/andrewrothstein/ansible-influxdb2)
+
+Installs InfluxDB2
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.influxdb2
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://dl.influxdata.com/influxdb/releases
+
+dl() {
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="influxdb2-${ver}_${platform}.${archive_type}"
+    # https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.3_linux_amd64.tar.gz
+    local url=$MIRROR/$file
+    local lfile=$DIR/$file
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver linux arm64
+}
+
+dl_ver ${1:-2.0.3}
