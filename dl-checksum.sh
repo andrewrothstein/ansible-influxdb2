2dc908a041cbb32cd9ebff2d2865c905183db05b
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-10-24 00:45:45 -0400

    refactor to include the cli too

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -3,14 +3,22 @@ set -e
 DIR=~/Downloads
 MIRROR=https://dl.influxdata.com/influxdb/releases
 
-dl() {
+dl_app() {
     local ver=$1
-    local os=$2
-    local arch=$3
-    local archive_type=${4:-tar.gz}
+    local app=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
     local platform="${os}-${arch}"
-    local file="influxdb2-${ver}-${platform}.${archive_type}"
-    # https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.6-linux-amd64.tar.gz
+    local file=""
+    if [ "$app" = "server" ];
+    then
+        file="influxdb2-${ver}-${platform}.${archive_type}"
+    else
+        file="influxdb2-client-${ver}-${platform}.${archive_type}"
+    fi
+    # https://dl.influxdata.com/influxdb/releases/influxdb2-2.4.0-linux-amd64.tar.gz
+    # https://dl.influxdata.com/influxdb/releases/influxdb2-client-2.4.0-linux-amd64.tar.gz
     local url=$MIRROR/$file
     local lfile=$DIR/$file
 
@@ -18,17 +26,36 @@ dl() {
     then
         curl -sSLf -o $lfile $url
     fi
-    printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    if [ -e $lfile ];
+    then
+        printf "      # %s\n" $url
+        printf "      %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    fi
+}
+
+dl_ver_app() {
+    local ver=$1
+    local app=$2
+    printf "    %s:\n" $app
+    dl_app $ver $app darwin amd64
+    dl_app $ver $app linux amd64
+    dl_app $ver $app linux arm64
+    dl_app $ver $app windows amd64 zip
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver darwin amd64
-    dl $ver linux amd64
-    dl $ver linux arm64
-    dl $ver windows amd64 zip
+    dl_ver_app $ver server
+    dl_ver_app $ver client
 }
 
+dl_ver 2.0.5
+dl_ver 2.0.6
+dl_ver 2.0.7
+dl_ver 2.0.8
+dl_ver 2.0.9
+dl_ver 2.1.1
+dl_ver 2.2.0
+dl_ver 2.3.0
 dl_ver ${1:-2.4.0}
