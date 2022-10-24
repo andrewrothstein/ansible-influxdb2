8c99fb49bbb8add414ed4be81aee7eeadbb8679a
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-10-23 23:58:17 -0400

    freshen. influxdb:2.4.0. change to ver structure.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -16,7 +16,7 @@ dl() {
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -28,6 +28,7 @@ dl_ver() {
     dl $ver darwin amd64
     dl $ver linux amd64
     dl $ver linux arm64
+    dl $ver windows amd64 zip
 }
 
-dl_ver ${1:-2.3.0}
+dl_ver ${1:-2.4.0}
